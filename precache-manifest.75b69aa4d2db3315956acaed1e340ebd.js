83835fb8da9a5bc224f6fb8bea36b45a2a7fdbc6
George Arhinful <georgearhinful442@gmail.com>
2019-03-21 11:21:47 +0000

    Updates

---
+++ precache-manifest.75b69aa4d2db3315956acaed1e340ebd.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "c3c1151678b76797ab8ddd8d18c1f122",
+    "url": "/school/static/media/logo.c3c11516.png"
+  },
+  {
+    "revision": "36e48b8d2f96deb83de5",
+    "url": "/school/static/css/main.787ad4d0.chunk.css"
+  },
+  {
+    "revision": "76db8dbb80672b8b283ec65514c182bd",
+    "url": "/school/static/media/service.76db8dbb.jpg"
+  },
+  {
+    "revision": "bcb631f6b86f04ca4099",
+    "url": "/school/static/js/1.bcb631f6.chunk.js"
+  },
+  {
+    "revision": "c8850233b83ec304c207",
+    "url": "/school/static/js/runtime~main.c8850233.js"
+  },
+  {
+    "revision": "1406ccc1b9f85bba2e99851120fedcee",
+    "url": "/school/static/media/pexels-photo-1007023.1406ccc1.jpeg"
+  },
+  {
+    "revision": "8f51b4d937476d9af75c3ca018eac59c",
+    "url": "/school/static/media/code1.8f51b4d9.jpg"
+  },
+  {
+    "revision": "36e48b8d2f96deb83de5",
+    "url": "/school/static/js/main.36e48b8d.chunk.js"
+  },
+  {
+    "revision": "ced5ab9b3dd1fa256d367e9f9fd6a209",
+    "url": "/school/static/media/admission.ced5ab9b.jpg"
+  },
+  {
+    "revision": "a655be2a0d98439548369298f583706e",
+    "url": "/school/static/media/pexels-photo-1305305.a655be2a.jpeg"
+  },
+  {
+    "revision": "b669071cd5aca35c68aa201055014d54",
+    "url": "/school/static/media/professional.b669071c.jpg"
+  },
+  {
+    "revision": "dd145241351f153da423ca03b8433ea2",
+    "url": "/school/static/media/contact.dd145241.jpeg"
+  },
+  {
+    "revision": "5a3d61c051f2643fce8e482f8420836c",
+    "url": "/school/static/media/about.5a3d61c0.jpeg"
+  },
+  {
+    "revision": "bcb631f6b86f04ca4099",
+    "url": "/school/static/css/1.63b4d792.chunk.css"
+  },
+  {
+    "revision": "196b22934eb66a2006b69a59b9918c2a",
+    "url": "/school/index.html"
+  }
+];
